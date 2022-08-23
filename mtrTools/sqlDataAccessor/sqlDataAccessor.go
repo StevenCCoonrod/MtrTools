b562ef1710f2dcc7ffca290fe4d57620ac68ed8e
Steve Coonrod <StevenCCoonrod@gmail.com>
2022-08-22 21:08:20 -0500

    Cred fix

--- mtrTools/sqlDataAccessor/sqlDataAccessor.go
+++ mtrTools/sqlDataAccessor/sqlDataAccessor.go
@@ -11,11 +11,12 @@ import (
 	_ "github.com/denisenkom/go-mssqldb"
 )
 
-// var server = "localhost"
-var server = "localhost\\MSSQLSERVER02"
+var server = "localhost"
+
+// var server = "localhost\\MSSQLSERVER02"
 var port = 1433
-var user = "stevec"
-var password = "3brahman3"
+var user = ""
+var password = ""
 var database = "NetopsToolsDB"
 
 var db *sql.DB
